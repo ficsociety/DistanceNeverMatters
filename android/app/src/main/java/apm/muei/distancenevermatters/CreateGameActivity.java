a67f9c26155d89e9ab58e0f3c5c7c2b87481f9c1
Shynfia <eva.suarez.garci@gmail.com>
2019-03-19 17:01:18 +0100

    Corregidos errores de márgenes en actividad CreateGame
    Algunos elementos no tenían restricciones hacia la derecha, lo que hacía que se salieran de la pantalla en algunos dispositivos. También se ha centrado el botón de crear

--- android/app/src/main/java/apm/muei/distancenevermatters/CreateGameActivity.java
+++ android/app/src/main/java/apm/muei/distancenevermatters/CreateGameActivity.java
@@ -8,6 +8,6 @@ public class CreateGameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.create_game);
+        setContentView(R.layout.activity_create_game);
     }
 }
