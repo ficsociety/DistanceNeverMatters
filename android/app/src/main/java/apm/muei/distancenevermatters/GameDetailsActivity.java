179d63c3e3e0bbb28e338da13116f6dd58514f00
jgpa <jose.poblado@hotmail.com>
2019-03-20 23:11:58 +0100

    Create gamedetailsactivity

---
+++ android/app/src/main/java/apm/muei/distancenevermatters/GameDetailsActivity.java
@@ -0,0 +1,14 @@
+package apm.muei.distancenevermatters;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class GameDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_game);
+
+    }
+}
