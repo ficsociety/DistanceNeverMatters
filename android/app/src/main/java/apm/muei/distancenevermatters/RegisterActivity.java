6dc92f5f51a56f9d2f18457565a43ef47fd6f4eb
Rodrigo Dopazo <roidopazo@gmail.com>
2019-03-21 15:40:30 +0100

    Toolbar en actividad de registro

--- android/app/src/main/java/apm/muei/distancenevermatters/RegisterActivity.java
+++ android/app/src/main/java/apm/muei/distancenevermatters/RegisterActivity.java
@@ -3,12 +3,14 @@ package apm.muei.distancenevermatters;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toolbar;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -20,10 +22,33 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         ButterKnife.bind(this);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CharSequence text = "Volviendo a login";
+                Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+                toast.show();
+                onBackPressed();
+            }
+        });
     }
 
-    public void skipToMain(View view) {
+    @OnClick(R.id.register)
+    public void onPressRegister(View view) {
+        register();
+    }
+
+    private void register() {
+        CharSequence text = "Registrando y logueando user...";
+        Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+        toast.show();
         Intent intent = new Intent(this, MainScreenActivity.class);
         startActivity(intent);
     }
+
+
 }
