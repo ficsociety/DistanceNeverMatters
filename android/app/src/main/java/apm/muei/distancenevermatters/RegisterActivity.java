31155a7c394c5eeff4b2e36a23649105d3c1c0c4
Rodrigo Dopazo <roidopazo@gmail.com>
2019-03-21 15:16:19 +0100

    Creada pantalla registro

--- android/app/src/main/java/apm/muei/distancenevermatters/RegisterActivity.java
+++ android/app/src/main/java/apm/muei/distancenevermatters/RegisterActivity.java
@@ -4,13 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    @BindView(R.id.register)
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
     }
 
     public void skipToMain(View view) {
