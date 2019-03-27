7901554a9894784d4c58a9ff07e6fc3ab236a897
Roi <roidopazo@gmail.com>
2019-03-28 00:01:26 +0100

    Añadida opción para habilitar la edición de una partida
    En la toolbar, se añadió la opción editar, se ha creado como un toggle con dos estados como una primera aproximación

--- android/app/src/main/java/apm/muei/distancenevermatters/GameDetailsFragment.java
+++ android/app/src/main/java/apm/muei/distancenevermatters/GameDetailsFragment.java
@@ -5,12 +5,36 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class GameDetailsFragment extends Fragment {
+
+    private boolean isChecked = false;
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.game_details_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.editGameDetails:
+                isChecked = item.isChecked();
+                item.setChecked(!isChecked);
+                System.out.println(item.isChecked());
+                return super.onOptionsItemSelected(item);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,6 +46,7 @@ public class GameDetailsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_game_details, container, false);
+        setHasOptionsMenu(true);
 
         Toolbar toolbar = getActivity().findViewById(R.id.mainToolbar);
         // Debería ser el nombre de la partida ¿?
@@ -56,8 +81,6 @@ public class GameDetailsFragment extends Fragment {
 
             }
         });
-
         return rootView;
     }
-
 }
