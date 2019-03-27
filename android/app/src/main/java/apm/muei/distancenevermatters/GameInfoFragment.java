afc36654e254ec558700604adc13e9621cc6b1b8
Shynfia <eva.suarez.garci@gmail.com>
2019-03-27 11:48:09 +0100

    Dividida pantalla de detalles de partida en dos fragmentos
    La parte correspondiente a la información de la partida propiamente dicha se ha movido a un fragmento anidado dentro del fragmento principal de la pantalla, que es el que también incluye los botones y el scroll. De este modo, podremos reutilizar el layout de la información de una partida en la pantalla de unirse a partida.

---
+++ android/app/src/main/java/apm/muei/distancenevermatters/GameInfoFragment.java
@@ -0,0 +1,20 @@
+package apm.muei.distancenevermatters;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class GameInfoFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game_info, container, false);
+    }
+
+}
