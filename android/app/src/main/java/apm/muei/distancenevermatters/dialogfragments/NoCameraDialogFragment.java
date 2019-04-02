919f8eafbe214fd7b3de9e568f613c4b88e5fb0f
Shynfia <eva.suarez.garci@gmail.com>
2019-04-02 13:39:33 +0200

    Añadida comprobación de disponibilidad de cámara
    Si el dispositivo no tiene cámara o una app que permita captura de vídeo, se muestra un diálogo en la pantalla de Login que avisa de esto. Al pulsar ok, cierra la app.
    Para probar este comportamiento en un dispositivo con cámara, cambiar las condiciones especificadas en el método checkCamera de LoginActivity.

---
+++ android/app/src/main/java/apm/muei/distancenevermatters/dialogfragments/NoCameraDialogFragment.java
@@ -0,0 +1,27 @@
+package apm.muei.distancenevermatters.dialogfragments;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+import apm.muei.distancenevermatters.R;
+
+public class NoCameraDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.no_camera)
+                .setMessage(R.string.camera_and_app_warning)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Ok then, nothing to do here
+                getActivity().finish();
+            }
+        });
+
+        return builder.create();
+    }
+}
