4f94b048f2fbda6258f05929876521bc596b25f5
Shynfia <eva.suarez.garci@gmail.com>
2019-04-12 11:25:44 +0200

    Actualizado APK
    También he quitado un PageAdapter del MainFragment que se había quedado ahí olvidado

--- android/app/src/main/java/apm/muei/distancenevermatters/fragments/MainFragment.java
+++ android/app/src/main/java/apm/muei/distancenevermatters/fragments/MainFragment.java
@@ -105,41 +105,6 @@ public class MainFragment extends Fragment {
         }
     }
 
-
-    public class GameTabsPagerAdapter extends FragmentPagerAdapter {
-
-        private Context mContext;
-
-        public GameTabsPagerAdapter(Context context, FragmentManager fm) {
-            super(fm);
-            mContext = context;
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return new GameListFragment();
-        }
-
-        @Override
-        public int getCount() {
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return mContext.getString(R.string.all);
-                case 1:
-                    return mContext.getString(R.string.master);
-                case 2:
-                    return mContext.getString(R.string.player);
-                default:
-                    return null;
-            }
-        }
-    }
-
     private void singOut() {
         // Firebase sign out
         gVars.getmAuth().signOut();
