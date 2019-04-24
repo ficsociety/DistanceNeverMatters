647f4c694997962d94aeffb69232a9d6f386c754
jgpa <jose.poblado@hotmail.com>
2019-04-24 20:36:51 +0200

    Implementada la descarga de marcadores.

--- android/app/src/main/java/apm/muei/distancenevermatters/adapters/MarkersRecyclerViewAdapter.java
+++ android/app/src/main/java/apm/muei/distancenevermatters/adapters/MarkersRecyclerViewAdapter.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -16,6 +17,11 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
 
 import apm.muei.distancenevermatters.R;
@@ -36,7 +42,7 @@ public class MarkersRecyclerViewAdapter extends RecyclerView.Adapter<MarkersRecy
     }
 
     public MarkersRecyclerViewAdapter(Context mContext, List<Marker> markers, Fragment parentFragment) {
-        this.markers =markers;
+        this.markers = markers;
         this.mContext = mContext;
         if (parentFragment instanceof OnItemDownloadSelected) {
             fragment = parentFragment;
@@ -65,9 +71,43 @@ public class MarkersRecyclerViewAdapter extends RecyclerView.Adapter<MarkersRecy
 
         viewHolder.name.setText(markers.get(position).getName());
 
-
         viewHolder.checkBox.setChecked(position == lastClicked);
 
+
+        viewHolder.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int positiondownload = viewHolder.getAdapterPosition();
+                String url = markers.get(positiondownload).getUrl().toString();
+                String name = markers.get(positiondownload).getName();
+                Context cxt = fragment.getActivity().getApplicationContext();
+                descargar(cxt, url, name);
+                Toast.makeText(cxt, "Descargando", Toast.LENGTH_LONG).show();
+
+            }
+
+            public void descargar(Context cxt, String urldownload, String name) {
+                try {
+                    URL url = new URL(urldownload);
+                    InputStream in = new BufferedInputStream(url.openStream());
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    byte[] buf = new byte[1024];
+                    int n = 0;
+                    while (-1 != (n = in.read(buf))) {
+                        out.write(buf, 0, n);
+                    }
+                    out.close();
+                    in.close();
+                    byte[] response = out.toByteArray();
+                    FileOutputStream outputStream = cxt.openFileOutput(name, Context.MODE_PRIVATE);
+                    outputStream.write(response);
+                    outputStream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
 
         viewHolder.image.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,25 +124,48 @@ public class MarkersRecyclerViewAdapter extends RecyclerView.Adapter<MarkersRecy
     }
 
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
         ImageView image;
         TextView name;
         CheckBox checkBox;
+        Button button;
 
-        public ViewHolder(View itemView){
+        public ViewHolder(View itemView) {
             super(itemView);
+
             ButterKnife.bind(this, itemView);
             image = itemView.findViewById(R.id.imageResourceGridDownload);
             name = itemView.findViewById(R.id.resourceNameGridDownload);
             checkBox = itemView.findViewById(R.id.checkBoxResourceGridDownload);
+            button = itemView.findViewById(R.id.buttonResourceGridDownload);
+
 
         }
 
-        @OnClick(R.id.buttonResourceGridDownload)
-        public void downloadMarker(){
-            Toast.makeText(fragment.getActivity().getApplicationContext(),
-                    "Descargando", Toast.LENGTH_LONG).show();
-        }
+//        @OnClick(R.id.buttonResourceGridDownload)
+
+
+//        public void descargar(Context cxt){
+//            try {
+//                URL url = new URL("https://i.imgur.com/8aeXtJD.png");
+//                InputStream in = new BufferedInputStream(url.openStream());
+//                ByteArrayOutputStream out = new ByteArrayOutputStream();
+//                byte[] buf = new byte[1024];
+//                int n = 0;
+//                while (-1!=(n=in.read(buf)))
+//                {
+//                    out.write(buf, 0, n);
+//                }
+//                out.close();
+//                in.close();
+//                byte[] response = out.toByteArray();
+//                FileOutputStream outputStream = cxt.openFileOutput("8aeXtJD.png", Context.MODE_PRIVATE);
+//                outputStream.write(response);
+//                outputStream.close();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
 
     }
 
