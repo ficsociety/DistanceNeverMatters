31107114ce855a7fc8afa2f50e75f2757727c773
Shynfia <eva.suarez.garci@gmail.com>
2019-05-05 14:44:53 +0200

    Añadida actividad para Unity
    Añadidad GameActivity, que contendrá la pantalla de Unity, y conectada con MainActivity a través de los botones de iniciar partida

---
+++ android/app/src/main/java/apm/muei/distancenevermatters/fragments/UnityFragment.java
@@ -0,0 +1,24 @@
+package apm.muei.distancenevermatters.fragments;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import apm.muei.distancenevermatters.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class UnityFragment extends Fragment {
+
+    public UnityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_unity, container, false);
+    }
+}
